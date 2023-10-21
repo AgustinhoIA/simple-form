89863f3cdbb421ba2f4d407db96f70715a3e67b2
AgustinhoIA <agustinhoigal@gmail.com>
2023-10-21 18:26:51 -0300

    Add valid and invalid css

--- simple-form.js
+++
@@ -1,9 +0,0 @@
-const myInput = document.getElementsByClassName('input');
-
-myInput.addEventListener('input', function() {
-    if (myInput.value !== '') {
-        myInput.classList.add('filled');
-    } else {
-        myInput.classList.remove('filled')
-    }  
-})
